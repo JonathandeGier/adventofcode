083d01b57ebca4d917743a25db3c64de25c7cd97
Jonathan de Gier <jonathan.de.gier@outlook.com>
2021-12-02 21:34:28 +0100

    add standard output to all days

--- 2021/day5.py
+++ 2021/day5.py
@@ -1,3 +1,13 @@
 from getInput import get_input
 
 input = get_input(2021, 5)
+
+
+
+print("Puzzle 1:")
+
+print("")
+
+
+
+print("Puzzle 2:")
--- 2021/day9.py
+++ 2021/day9.py
@@ -1,3 +1,13 @@
 from getInput import get_input
 
 input = get_input(2021, 9)
+
+
+
+print("Puzzle 1:")
+
+print("")
+
+
+
+print("Puzzle 2:")
