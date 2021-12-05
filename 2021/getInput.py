8cabc7195f57eb71536b54d585e8926cfe48ab28
Jonathan de Gier <jonathan.de.gier@outlook.com>
2021-12-05 09:55:41 +0100

    save input to file so AoC is only called once

--- 2021/getInput.py
+++ 2021/getInput.py
@@ -1,10 +1,23 @@
 import requests
+import os
 
 def get_input(year, day):
+
+    file_location = str(year) + "/input/input" + str(day) + ".txt"
+
+    if os.path.isfile(file_location):
+        print("get from file")
+        return open(file_location, "r").read()
+
     session = open("session", "r").read()
 
     url = "https://adventofcode.com/" + str(year) + "/day/" + str(day) + "/input"
 
     text = requests.get(url, cookies={"session": session}).text
+
+    print("get from url")
+    file = open(file_location, "w")
+    file.write(text)
+    file.close()
 
     return text
