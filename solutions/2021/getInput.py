23c80a6fe0b94acf4ba63d5180da98f732e6dc7d
Jonathan de Gier <jonathan.de.gier@outlook.com>
2021-12-06 20:34:35 +0100

    change to correct directory

--- solutions/2021/getInput.py
+++ solutions/2021/getInput.py
@@ -3,12 +3,12 @@ import os
 
 def get_input(year, day):
 
-    file_location = "input/input" + str(day) + ".txt"
+    file_location = "solutions/" + str(year) + "/input/input" + str(day) + ".txt"
 
     if os.path.isfile(file_location):
         return open(file_location, "r").read()
 
-    session = open("../../session", "r").read()
+    session = open("session", "r").read()
 
     url = "https://adventofcode.com/" + str(year) + "/day/" + str(day) + "/input"
 
