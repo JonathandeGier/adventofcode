94482783ed32e441d8972bceee3e1ea8683303cd
Jonathan de Gier <jonathan.de.gier@outlook.com>
2021-12-11 10:43:53 +0100

    day 11

--- solutions/2021/day11.py
+++ solutions/2021/day11.py
@@ -2,24 +2,7 @@ from os import truncate
 from getInput import get_input
 
 def get_data():
-    example = """11111
-19991
-19191
-19991
-11111"""
-
-    example2 = """5483143223
-2745854711
-5264556173
-6141336146
-6357385478
-4167524645
-2176841721
-6882881134
-4846848554
-5283751526"""
     input = get_input(2021, 11).splitlines()
-    # input = example2.splitlines()
     data = []
     for row in input:
         row = [int(val) for val in row]
