edb7a0266126ba3099e2727ad70af0f701fe1b2e
Jonathan de Gier <jonathan.de.gier@outlook.com>
2022-12-20 21:04:04 +0100

    2022 day 20

--- solutions/2022/day20.py
+++ solutions/2022/day20.py
@@ -1,6 +1,14 @@
 from Table import Table
 from time import time
 
+class Item:
+    def __init__(self, index: int, value: int):
+        self.index = index
+        self.value = value
+
+        self.next = None
+        self.prev = None
+
 class Day20(Table):
 
     def __init__(self):
@@ -8,76 +16,140 @@ class Day20(Table):
         self.title = "Grove Positioning System"
         self.input = self.getInput(self.day)
 
-    def mix(self, list: list, rounds: int = 1):
+        self.items = {}
+        self.length = None
+        self.zero = None
+
+    # def mix(self, list: list, rounds: int = 1):
+    #     for _ in range(rounds):
+    #         for i in range(len(list)):
+    #             item = [item for item in list if item['org_index'] == i][0]
+
+    #             _from = item['new_index']
+    #             new_index = _from + item['value']
+                
+    #             # index 0 and index -1 are the same, so for each time we loop around we need to shift an extra place
+    #             shift = new_index // len(list)
+
+    #             shift_dir = shift // len(list)
+
+    #             shift_dir_dir = shift_dir // len(list)
+    #             _to = (new_index + shift + shift_dir + shift_dir_dir) % len(list)
+    #             # print(shift_dir_dir)
+                
+    #             # _to = new_index % len(list)
+    #             # _to += (new_index // len(list))
+    #             # _to = _to % len(list)
+
+
+    #             if _to >_from:
+    #                 # move everything in between forward
+    #                 for moving_item in [moving_item for moving_item in list if moving_item['new_index'] > _from and moving_item['new_index'] <= _to]:
+    #                     moving_item['new_index'] -= 1
+
+    #             elif _to < _from:
+    #                 # move everything in between backward
+    #                 for moving_item in [moving_item for moving_item in list if moving_item['new_index'] >= _to and moving_item['new_index'] < _from]:
+    #                     moving_item['new_index'] += 1
+                
+    #             item['new_index'] = _to
+
+    #         # for i in range(len(list)):
+    #         #     item = [item for item in list if item['new_index'] == i][0]
+    #         #     print(item['value'], end=' ')
+    #         print('mixed')
+
+    #     return list
+
+    def load_items(self, p2 = False):
+        self.items = {}
+        for i, val in enumerate(self.input.splitlines()):
+            value = int(val)
+
+            if p2:
+                value = value * 811589153
+            self.items[i] = Item(i, value)
+
+            if value == 0:
+                self.zero = self.items[i]
+
+        self.length = len(self.items.keys())
+        for i in range(self.length):
+            next_i = (i + 1) % self.length
+            prev_i = (i - 1) % self.length
+
+            self.items[i].next = self.items[next_i]
+            self.items[i].prev = self.items[prev_i]
+
+    def mix(self, rounds: int = 1):
         for _ in range(rounds):
-            for i in range(len(list)):
-                item = [item for item in list if item['org_index'] == i][0]
+            for i in range(self.length):
 
-                _from = item['new_index']
-                new_index = _from + item['value']
-                
-                # index 0 and index -1 are the same, so for each time we loop around we need to shift an extra place
-                shift = new_index // len(list)
+                item = self.items[i]
 
-                shift_dir = shift // len(list)
+                if item.value == 0:
+                    continue
 
-                shift_dir_dir = shift_dir // len(list)
-                _to = (new_index + shift + shift_dir + shift_dir_dir) % len(list)
-                # print(shift_dir_dir)
-                
-                # _to = new_index % len(list)
-                # _to += (new_index // len(list))
-                # _to = _to % len(list)
+                diff = abs(item.value) % (self.length - 1)
+                assert diff != 0
+
+                other_item = item
+                for _ in range(diff):
+                    if item.value > 0:
+                        other_item = other_item.next
+                    else:
+                        other_item = other_item.prev
+
+                if item.value > 0:
+                    other_item = other_item.next
 
 
-                if _to >_from:
-                    # move everything in between forward
-                    for moving_item in [moving_item for moving_item in list if moving_item['new_index'] > _from and moving_item['new_index'] <= _to]:
-                        moving_item['new_index'] -= 1
+                # move the item to the other item
+                # remove the item from its current position
+                item.next.prev = item.prev
+                item.prev.next = item.next
 
-                elif _to < _from:
-                    # move everything in between backward
-                    for moving_item in [moving_item for moving_item in list if moving_item['new_index'] >= _to and moving_item['new_index'] < _from]:
-                        moving_item['new_index'] += 1
-                
-                item['new_index'] = _to
+                # insert the item adter the other_item
+                temp_other_prev = other_item.prev
+                temp_other_next = other_item.next
+                other_item.prev = item
+                item.next = other_item
+                item.prev = temp_other_prev
+                temp_other_prev.next = item
 
-            # for i in range(len(list)):
-            #     item = [item for item in list if item['new_index'] == i][0]
-            #     print(item['value'], end=' ')
-            print('mixed')
-
-        return list
+    def printList(self):
+        current = self.zero
+        for _ in range(self.length):
+            print(current.value, end=' ')
+            current = current.next
+        print('')
 
     def solve(self):
         start_time = time()
 
-        # not -7271027221727
-        # not -5152779532397
-        # not 7317287803448
-        # ... < x < 8288760019589
-        # print((1567990243596) + (7779893620658) + (-1059123844665))
+        self.printRow((self.day, self.title, '', '', ''), end='\r')
 
-        list = [{'org_index': i, 'new_index': i, 'value': int(val) * 811589153} for i, val in enumerate(self.input.splitlines())]
-
-        # for i in range(len(list)):
-        #     item = [item for item in list if item['new_index'] == i][0]
-        #     print(item['value'], end=' ')
-        # print('')
-
-        list = self.mix(list, 10)
+        self.load_items()
+        self.mix()
 
         part1 = 0
-        zero = [item for item in list if item['value'] == 0][0]
-        print(zero)
-        for i in [1000, 2000, 3000]:
-            index = (zero['new_index'] + i) % len(list)
+        current = self.zero
+        for _ in range(3):
+            for _ in range(1000):
+                current = current.next
+            part1 += current.value
 
-            value = [item for item in list if item['new_index'] == index][0]
-            print(value)
-            part1 += value['value']
+        self.printRow((self.day, self.title, part1, '', ''), end='\r')
 
-        part2 = "None"
+        self.load_items(True)
+        self.mix(10)
+
+        part2 = 0
+        current = self.zero
+        for _ in range(3):
+            for _ in range(1000):
+                current = current.next
+            part2 += current.value
 
         end_time = time()
         seconds_elapsed = end_time - start_time
