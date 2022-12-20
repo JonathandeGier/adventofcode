6494787f5af9d91c7cca124c26caef56f3f14766
Jonathan de Gier <jonathan.de.gier@outlook.com>
2022-12-20 21:04:50 +0100

    2022 day 20

--- solutions/2022/day20.py
+++ solutions/2022/day20.py
@@ -20,47 +20,6 @@ class Day20(Table):
         self.length = None
         self.zero = None
 
-    # def mix(self, list: list, rounds: int = 1):
-    #     for _ in range(rounds):
-    #         for i in range(len(list)):
-    #             item = [item for item in list if item['org_index'] == i][0]
-
-    #             _from = item['new_index']
-    #             new_index = _from + item['value']
-                
-    #             # index 0 and index -1 are the same, so for each time we loop around we need to shift an extra place
-    #             shift = new_index // len(list)
-
-    #             shift_dir = shift // len(list)
-
-    #             shift_dir_dir = shift_dir // len(list)
-    #             _to = (new_index + shift + shift_dir + shift_dir_dir) % len(list)
-    #             # print(shift_dir_dir)
-                
-    #             # _to = new_index % len(list)
-    #             # _to += (new_index // len(list))
-    #             # _to = _to % len(list)
-
-
-    #             if _to >_from:
-    #                 # move everything in between forward
-    #                 for moving_item in [moving_item for moving_item in list if moving_item['new_index'] > _from and moving_item['new_index'] <= _to]:
-    #                     moving_item['new_index'] -= 1
-
-    #             elif _to < _from:
-    #                 # move everything in between backward
-    #                 for moving_item in [moving_item for moving_item in list if moving_item['new_index'] >= _to and moving_item['new_index'] < _from]:
-    #                     moving_item['new_index'] += 1
-                
-    #             item['new_index'] = _to
-
-    #         # for i in range(len(list)):
-    #         #     item = [item for item in list if item['new_index'] == i][0]
-    #         #     print(item['value'], end=' ')
-    #         print('mixed')
-
-    #     return list
-
     def load_items(self, p2 = False):
         self.items = {}
         for i, val in enumerate(self.input.splitlines()):
