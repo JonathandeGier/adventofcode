78c643543d15aa21116e195cb75083ca63145b34
Jonathan de Gier <jonathan.de.gier@outlook.com>
2024-12-12 20:43:33 +0100

    2024 day 12 part 2

--- solutions/2024/day12.py
+++ solutions/2024/day12.py
@@ -2,6 +2,40 @@ from Table import Table
 from time import time
 from collections import deque
 
+COLORS = {
+    'A': (96,68,57),
+    'B': (158,154,117),
+    'C': (28,34,46),
+    'D': (65,83,59),
+    'E': (85,72,64),
+
+    'F': (43,49,10),
+    'G': (75,83,32),
+    'H': (106,115,55),
+    'I': (146,154,104),
+    'J': (169,175,139),
+
+    'K': (230,229,202),
+    'L': (211,209,170),
+    'M': (189,185,136),
+    'N': (169,165,108),
+    'O': (118,116,66),
+
+    'P': (198,214,204),
+    'Q': (124,152,129),
+    'R': (78,107,77),
+    'S': (46,64,40),
+    'T': (38,47,27),
+
+    'U': (31,55,47),
+    'V': (47,60,40),
+    'W': (56,59,38),
+    'X': (61,58,36),
+    'Y': (69,57,35),
+
+    'Z': (96,68,57),
+}
+
 class Day12(Table):
 
     def __init__(self):
@@ -66,32 +100,26 @@ class Day12(Table):
 
                     queue.append(new_pos)
             
-            print(self.map[pos])
             sides = 0
             for dir, segments in perimeter_segments.items():
                 edge = 1 if dir[0] == 0 else 0
                 val = 0 if dir[0] == 0 else 1
-                print(dir, segments)
                 
                 sides += 1
-                segments.sort(key=lambda pos: pos[edge])
-                print(dir,segments)
+                segments.sort(key=lambda pos: (pos[edge], pos[val]))
                 for i in range(1, len(segments)):
                     if segments[i][edge] != segments[i-1][edge]:
                         sides += 1
                         continue
 
-                    print(abs(segments[i][val] - segments[i-1][val]))
                     if abs(segments[i][val] - segments[i-1][val]) != 1:
-                        pass
-                    else:
                         sides += 1
 
-            print(sides)
-            exit()
             part1 += region_area * region_perimeter
-            # print(self.map[pos], region_area, region_perimeter)
+            part2 += region_area * sides
 
+
+        self.image_map(self.map, COLORS, scale=10).save(self.visual_path('field.png'))
 
         end_time = time()
         seconds_elapsed = end_time - start_time
