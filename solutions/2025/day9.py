8add7b7d3998588c5246e68725818c3a12da621a
Jonathan de Gier <jonathan.de.gier@outlook.com>
2025-12-09 23:27:03 +0100

    2025 day 9 part 2

--- solutions/2025/day9.py
+++ solutions/2025/day9.py
@@ -17,11 +17,8 @@ class Day9(Table):
         
         pairs = []
         for i in range(len(red_tiles)):
-            next_i = i+1
-            if next_i == len(red_tiles):
-                i = 0
-
-            start, end = red_tiles[i], red_tiles[i+1]
+            next_i = (i+1) % len(red_tiles)
+            start, end = red_tiles[i], red_tiles[next_i]
 
             # Point is on a line between red tiles
             if start[0] == point[0] and end[0] == point[0]:
@@ -44,15 +41,37 @@ class Day9(Table):
 
         return len(pairs) % 2 == 1
     
-    def line_in_shape(self, start, end, red_tiles):
-        if start[0] == end[0]:
-            # vertical line, search for crossing horizontal lines
-            pass
-        elif start[1] == end[1]:
-            # horizontal line, search for crossing vertical lines
-            pass
-        else:
-            assert False, 'Line on a diagonal'
+    def line_in_shape(self, line_start, line_end, red_tiles):
+        for i in range(len(red_tiles)):
+            next_i = (i+1) % len(red_tiles)
+            start, end = red_tiles[i], red_tiles[next_i]
+
+            if line_start[0] == line_end[0]:
+                # vertical line, search for crossing horizontal lines
+                if start[1] == end[1] and min(start[0], end[0]) < line_start[0] and line_start[0] < max(start[0], end[0]) \
+                    and min(line_start[1], line_end[1]) < start[1] and start[1] < max(line_start[1], line_end[1]):
+
+                    return False
+            elif line_start[1] == line_end[1]:
+                # horizontal line, search for crossing vertical lines
+                if start[0] == end[0] and min(start[1], end[1]) < line_start[1] and line_start[1] < max(start[1], end[1]) \
+                    and min(line_start[0], line_end[0]) < start[0] and start[0] < max(line_start[0], line_end[0]):
+
+                    return False
+            else:
+                assert False, 'Line on a diagonal'
+
+        return True
+    
+    def no_red_tile_in_shape(self, corner1, corner2, red_tiles) -> bool:
+        min_corner = (min(corner1[0], corner2[0]), min(corner1[1], corner2[1]))
+        max_corner = (max(corner1[0], corner2[0]), max(corner1[1], corner2[1]))
+
+        for tile in red_tiles:
+            if min_corner[0] < tile[0] and tile[0] < max_corner[0] and min_corner[1] < tile[1] and tile[1] < max_corner[1]:
+                return False
+            
+        return True
 
 
     def solve(self):
@@ -69,24 +88,26 @@ class Day9(Table):
             area = (abs(end[0] - start[0]) + 1) * (abs(end[1] - start[1]) + 1)
             part1 = max(part1, area)
 
+            # other 2 corners of the square
             other_1 = (start[0], end[1])
             other_2 = (end[0], start[1])
 
-            # if start == (11, 1) and end == (2, 5):
-            #     print(other_1, other_2)
-            #     print(self.on_line(other_1, tiles), self.in_shape(other_1, tiles))
-            #     print(self.on_line(other_2, tiles), self.in_shape(other_2, tiles))
-            #     print((self.on_line(other_1, tiles) or self.in_shape(other_1, tiles)) and (self.on_line(other_2, tiles) or self.in_shape(other_2, tiles)))
+            # All edges of the square
+            line1 = (start, other_1)
+            line2 = (start, other_2)
+            line3 = (end, other_1)
+            line4 = (end, other_2)            
 
+            if self.on_green_tile(other_1, tiles) and \
+                self.on_green_tile(other_2, tiles) and \
+                self.line_in_shape(line1[0], line1[1], tiles) and \
+                self.line_in_shape(line2[0], line2[1], tiles) and \
+                self.line_in_shape(line3[0], line3[1], tiles) and \
+                self.line_in_shape(line4[0], line4[1], tiles) and \
+                self.no_red_tile_in_shape(start, end, tiles):
 
-            if self.on_green_tile(other_1, tiles) and self.on_green_tile(other_2, tiles):
-                # part2 = max(part2, area)
-                if area > part2:
-                    part2 = area
-                    print(start, end, area)
+                part2 = max(part2, area)
 
-
-        # ... < x < 4591195600
         end_time = time()
         seconds_elapsed = end_time - start_time
 
